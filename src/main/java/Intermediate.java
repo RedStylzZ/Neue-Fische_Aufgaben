297cb4204c48f0c9bade6e83b4aa65ae4108b47c
Tizian <tizianappler@yahoo.de>
2021-12-02 16:01:36 +0100

    Finished Intermediate

--- src/main/java/Intermediate.java
+++ src/main/java/Intermediate.java
@@ -4,7 +4,7 @@ public class Intermediate {
     static final String[] numbers = {"Eins", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun", "Zehn"};
 
     public static String getNumWord(int num) {
-        return numbers[num-1];
+        return numbers[num];
     }
 
     public static void printNumbers() {
@@ -24,26 +24,40 @@ public class Intermediate {
 
     public static void inputNames() {
         String name = "";
-        String[] names = {""};
+        String[] names = new String[1];
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
 
-        while(true) {
-            for (String s: names ) {
-                System.out.println(s);
+        while (true){
+            if (count>=names.length){
+                String [] tmp = new String [names.length+1];
+                System.arraycopy(names, 0, tmp, 0, names.length);
+                names = tmp;
             }
 
-            
+            System.out.print("Name: ");
+            name = scanner.nextLine();
+            if(name.equals("exit")) {
+                break;
+            }
+            names[count]=name;
+            count++;
+
+            System.out.println();
+            for (String x : names) {
+                System.out.println(x);
+            }
         }
+
     }
 
     public static void main(String[] args) {
 
-        for (int x = 1; x <= 9; x++) {
+        for (int x = 0; x <= 8; x++) {
             System.out.println(getNumWord(x));
         }
 
         printNumbers();
-
         inputNames();
     }
 
